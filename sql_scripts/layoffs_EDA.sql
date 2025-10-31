db50e182e9aa7fe87d01dbda648e33bf1a26c834
Justin <khujustin@gmail.com>
2025-10-31 17:22:11 +1100

    Add files via upload

---
+++ sql_scripts/layoffs_EDA.sql
@@ -0,0 +1,37 @@
+USE world_layoff;
+
+-- Layoffs by month 
+
+SELECT SUBSTRING(`date`, 1,7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY `Month`
+ORDER BY 1 ;
+
+-- Creating a rolling total for people laid off
+
+WITH RollingTotal AS (
+	SELECT SUBSTRING(`date`, 1,7) AS `Month`, SUM(total_laid_off) AS TotalLaid
+	FROM layoffs_staging2
+	GROUP BY `Month`
+) 
+SELECT 
+	`Month`, TotalLaid, 
+	SUM(TotalLaid) OVER (ORDER BY `Month`) AS rolling_total
+FROM RollingTotal ; 
+
+-- Top 5 companies by layoff for each year
+
+WITH Ranked_Layoffs AS 
+(
+	SELECT company, industry, YEAR(`date`) AS years, SUM(total_laid_off) AS total_laid_off
+	FROM layoffs_staging2 
+	GROUP BY company, industry, years
+)
+, Company_year_rank AS 
+(
+SELECT *, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS Ranked_Layoffs_by_year
+FROM Ranked_layoffs 
+)
+SELECT * 
+FROM Company_year_rank 
+WHERE Ranked_Layoffs_by_year <= 5 ;
